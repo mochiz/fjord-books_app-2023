f4cd5650f28d9ae25e1036344abb1577a378395c
mochiz <mochi.hiro43@gmail.com>
2024-11-03 12:21:25 +0900

    開発環境でのメール確認のため letter_opener_web をインストールした

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
