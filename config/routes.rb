76f3323329961986c64f9a8c6a44d566ad0964a4
mochiz <mochi.hiro43@gmail.com>
2024-11-03 12:32:50 +0900

    ユーザー一覧、詳細ページを追加した

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: %i[index show]
   resources :books
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root 'users#index'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
