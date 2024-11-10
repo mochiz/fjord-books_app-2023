43833932dd549fe4a112b189c3af8e6cde5191a9
mochiz <mochi.hiro43@gmail.com>
2024-11-10 23:20:55 +0900

    トップページを本の一覧に差し替えた

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :users, only: %i[index show]
   resources :books
 
-  root 'users#index'
+  root 'books#index'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
